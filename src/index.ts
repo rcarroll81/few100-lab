e61efe26dedc4b36a3742455e0e21f774c5b65d3
rcarroll81 <50146997+rcarroll81@users.noreply.github.com>
2019-05-20 13:47:18 -0400

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,50 @@
-
 import './styles.css';
-console.log('Ready to Party');
+
+const num = document.getElementById('num') as HTMLInputElement;
+const billAmount = document.getElementById('bill-amount') as HTMLSpanElement;
+const tenPercent = document.getElementById('ten') as HTMLButtonElement;
+const fifteenPercent = document.getElementById('fifteen') as HTMLButtonElement;
+const twentyPercent = document.getElementById('twenty') as HTMLButtonElement;
+const tipPercent = document.getElementById('tip-percent') as HTMLSpanElement;
+const tipAmount = document.getElementById('tip-amount') as HTMLSpanElement;
+const totalBill = document.getElementById('bill-total') as HTMLSpanElement;
+const tipButtons = document.querySelectorAll('#amount-buttons button');
+tipButtons.forEach((el)=> el.addEventListener('click', setUp));
+
+num.addEventListener('keyup', updateUi);
+
+function updateUi() {
+    billAmount.innerText = '$' + this.value; // Format this.
+    const amountOfTip = (currentAmountForTip / 100) * +this.value;
+    tipAmount.innerHTML = '$' + amountOfTip.toString();
+    const amountOfBill = amountOfTip + (+this.value);
+    totalBill.innerHTML = '$' + amountOfBill.toString();
+}
+
+let currentAmountForTip = 20;
+ function setUp(){
+    tipPercent.innerText = this.dataset.amount + '%';
+    currentAmountForTip = +this.dataset.amount;    
+    tipButtons.forEach(button => {
+        const clickedButton = button as HTMLButtonElement;
+        if(this == button) {
+            clickedButton.disabled = true;
+        } else {
+            clickedButton.disabled = false;
+        }
+
+       updateUi.call(num);
+    })
+};
+
+/*
+When the amount changes
+take the currentAmountToTip
+    convert it to a percentage
+multiply it by the amount
+show that as the tip amount
+add that tip amount to the amount for the total bill
+
+
+*/
+
